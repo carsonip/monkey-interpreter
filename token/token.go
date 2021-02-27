5d9801770fd9e5af7787a67c5dc75f49b1ceda92
Carson Ip <carsonip715@gmail.com>
2021-02-27 12:11:19 +0000

    Multi char token

--- token/token.go
+++ token/token.go
@@ -4,6 +4,7 @@ type TokenType int
 
 const (
 	_ = iota
+	TOKEN_ILLEGAL
 	TOKEN_EOF
 	TOKEN_LPAREN
 	TOKEN_RPAREN
@@ -18,6 +19,18 @@ const (
 	TOKEN_SQUOTE
 	TOKEN_DQOUTE
 	TOKEN_IDENTIFIER
+	TOKEN_NUMBER
+	TOKEN_FUNCTION
+	TOKEN_LET
+	TOKEN_TRUE
+	TOKEN_FALSE
+	TOKEN_IF
+	TOKEN_ELSE
+	TOKEN_RETURN
+	TOKEN_EQUAL
+	TOKEN_NOTEQUAL
+	TOKEN_ASSIGNMENT
+	TOKEN_NOT
 )
 
 var charToToken = map[byte]TokenType{
@@ -34,6 +47,16 @@ var charToToken = map[byte]TokenType{
 	'}': TOKEN_RBRACE,
 	'\'': TOKEN_SQUOTE,
 	'"': TOKEN_DQOUTE,
+}
+
+var keywords = map[string]TokenType{
+	"fn": TOKEN_FUNCTION,
+	"let": TOKEN_LET,
+	"true": TOKEN_TRUE,
+	"false": TOKEN_FALSE,
+	"if": TOKEN_IF,
+	"else": TOKEN_ELSE,
+	"return": TOKEN_RETURN,
 }
 
 type Token struct {
@@ -62,10 +85,28 @@ func (l *Lexer) readChar() {
 	}
 }
 
-func (l *Lexer) readIdentifier() {
-	for l.ch != ' ' {
+func (l *Lexer) peekChar() byte {
+	if l.pos+1 >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.pos+1]
+	}
+}
+
+func (l *Lexer) readIdentifier() string {
+	lastPos := l.pos
+	for l.pos < len(l.input) && l.ch != ' ' {
 		l.readChar()
 	}
+	return l.input[lastPos:l.pos]
+}
+
+func (l *Lexer) readNumber() string {
+	lastPos := l.pos
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[lastPos:l.pos]
 }
 
 func (l *Lexer) eatWhitespace() {
@@ -77,17 +118,59 @@ func (l *Lexer) eatWhitespace() {
 func (l *Lexer) NextToken() Token {
 	l.eatWhitespace()
 	if isAlpha(l.ch) {
-		lastPos := l.pos
-		l.readIdentifier()
+		str := l.readIdentifier()
+		var tokenType TokenType
+		if keywordType, ok := keywords[str]; ok {
+			tokenType = keywordType
+		} else {
+			tokenType = TOKEN_IDENTIFIER
+		}
 		return Token{
-			tokenType: TOKEN_IDENTIFIER,
-			literal: l.input[lastPos:l.pos],
+			tokenType: tokenType,
+			literal: str,
+		}
+	} else if isDigit(l.ch) {
+		str := l.readNumber()
+		return Token{
+			tokenType: TOKEN_NUMBER,
+			literal: str,
+		}
+	} else if l.ch == '=' {
+		l.readChar()
+		if l.ch == '=' {
+			l.readChar()
+			return Token{
+				tokenType: TOKEN_EQUAL,
+				literal: "==",
+			}
+		} else {
+			return Token{
+				tokenType: TOKEN_ASSIGNMENT,
+				literal: "=",
+			}
+		}
+	} else if l.ch == '!' {
+		l.readChar()
+		if l.ch == '=' {
+			l.readChar()
+			return Token{
+				tokenType: TOKEN_NOTEQUAL,
+				literal: "!=",
+			}
+		} else {
+			return Token{
+				tokenType: TOKEN_NOT,
+				literal: "!",
+			}
 		}
 	} else {
 		tokenType, ok := charToToken[l.ch]
 		l.readChar()
 		if !ok {
-			panic("unknown token")
+			return Token{
+				tokenType: TOKEN_ILLEGAL,
+				literal: "",
+			}
 		}
 		return Token{
 			tokenType: tokenType,
@@ -98,4 +181,8 @@ func (l *Lexer) NextToken() Token {
 
 func isAlpha(b byte) bool {
 	return b >= 'A' && b <= 'Z' || b >= 'a' && b <= 'z'
+}
+
+func isDigit(b byte) bool {
+	return b >= '0' && b <= '9'
 }
--- token/token_test.go
+++ token/token_test.go
@@ -3,21 +3,48 @@ package token
 import "testing"
 
 func TestLexer_NextToken(t *testing.T) {
-	l := NewLexer(",. let .")
+	var tok Token
+	l := NewLexer(",. foo let 0123 == != = !foo")
 	if l.NextToken().tokenType != TOKEN_COMMA {
 		t.Error()
 	}
 	if l.NextToken().tokenType != TOKEN_DOT {
 		t.Error()
 	}
-	tok := l.NextToken()
+	tok = l.NextToken()
 	if tok.tokenType != TOKEN_IDENTIFIER {
+		t.Error()
+	}
+	if tok.literal != "foo" {
+		t.Error()
+	}
+	tok = l.NextToken()
+	if tok.tokenType != TOKEN_LET {
 		t.Error()
 	}
 	if tok.literal != "let" {
 		t.Error()
 	}
-	if l.NextToken().tokenType != TOKEN_DOT {
+	tok = l.NextToken()
+	if tok.tokenType != TOKEN_NUMBER {
+		t.Error()
+	}
+	if tok.literal != "0123" {
+		t.Error()
+	}
+	if l.NextToken().tokenType != TOKEN_EQUAL {
+		t.Error()
+	}
+	if l.NextToken().tokenType != TOKEN_NOTEQUAL {
+		t.Error()
+	}
+	if l.NextToken().tokenType != TOKEN_ASSIGNMENT {
+		t.Error()
+	}
+	if l.NextToken().tokenType != TOKEN_NOT {
+		t.Error()
+	}
+	if l.NextToken().tokenType != TOKEN_IDENTIFIER {
 		t.Error()
 	}
 	if l.NextToken().tokenType != TOKEN_EOF {
